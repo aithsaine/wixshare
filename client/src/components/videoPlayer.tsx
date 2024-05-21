287615f908a8d32834823e65f640f69e1293ddb9
ismail ait hsaine <92794718+aithsaine@users.noreply.github.com>
2024-05-21 15:56:13 +0100

    some updates for notifications feature

--- client/src/components/videoPlayer.tsx
+++ client/src/components/videoPlayer.tsx
@@ -4,25 +4,18 @@ import ReactPlayer from 'react-player';
 export default function VideoPlayer({ file }: any) {
     const playerRef = useRef<any>(null);
 
-    const handleFocus = () => {
-        if (playerRef.current) {
-            const player = playerRef.current?.getInternalPlayer();
-            if (player) {
-                player.play();
-            }
-        }
-    };
+
 
     return (
         <div>
             {file && (
                 <video
                     src={file}
-                    controls={true}
                     width="100%"
+                    className='rounded-xl'
                     height="auto"
-                    tabIndex={0} // Ensure the element is focusable
-                    onFocus={handleFocus}
+                    controls
+                    muted
                 />
             )}
         </div>
